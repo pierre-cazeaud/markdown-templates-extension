eb56659c3732e40fec642bb9a10b55d9bd692b6d
Pierre Cazeaud <pcazeaud@turbulent.ca>
2024-03-20 09:10:51 -0400

    feat: Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,svelte,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ wxt.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'wxt';
+import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  srcDir: 'src',
+  manifestVersion: 3,
+  vite: () => ({
+    plugins: [
+      svelte({
+        // Using a svelte.config.js file causes a segmentation fault when importing the file
+        configFile: false,
+        preprocess: [vitePreprocess()],
+      }),
+    ],
+  }),
+});
