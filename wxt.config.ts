7603aa88496233ad160d0ce7e75d87e60ca29a51
Pierre Cazeaud <pcazeaud@turbulent.ca>
2024-03-21 20:26:19 -0400

    feat: Add workaround to configure options page in manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'wxt';
 import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { readFile, writeFile } from 'fs';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: 'src',
+  hooks: {
+    'build:manifestGenerated': (wxt, manifest) => {
+      // Modify manifest and allow options page to open in a new tab
+      manifest.options_ui.open_in_tab = true;
+    },
+  },
   manifestVersion: 3,
   vite: () => ({
     plugins: [
