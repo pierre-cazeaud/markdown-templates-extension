3b76d40cf5357eb462ee9bc693a17e3a021c88a3
Pierre Cazeaud <pcazeaud@turbulent.ca>
2024-03-25 16:40:43 -0400

    feat: Adjust TW colors and add button variants

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -2,20 +2,26 @@
   import type { ComponentType } from 'svelte';
   import type { Icon } from 'lucide-svelte';
 
-  type ButtonColorVariant = 'primary' | 'secondary';
+  type ButtonColorVariant =
+    | 'primary'
+    | 'secondary'
+    | 'destructive'
+    | 'interactive';
 
   type Props = {
     colorVariant?: ButtonColorVariant;
     icon?: ComponentType<Icon>;
-    sizeVariant?: 'small';
     onClick: () => void;
+    sizeVariant?: 'small';
+    title?: string;
   };
 
   let {
     colorVariant = 'primary',
     icon,
-    sizeVariant,
     onClick,
+    sizeVariant,
+    title,
   }: Props = $props();
 
   const colorClasses: Record<ButtonColorVariant, string> = {
@@ -23,12 +29,17 @@
       'px-4 py-2 bg-gradient-to-tr from-primary to-pink-600 text-on-primary transition hover:hue-rotate-15 hover:text-hover-on-primary',
     secondary:
       'p-2 text-secondary hover:bg-hover-secondary hover:text-hover-on-secondary',
+    destructive:
+      'p-2 bg-destructive text-on-destructive hover:bg-hover-destructive hover:text-hover-on-destructive',
+    interactive:
+      'p-2 bg-interactive text-on-interactive hover:bg-hover-interactive hover:text-hover-on-interactive',
   };
 </script>
 
 <button
   onclick={onClick}
   class={`flex items-center gap-1 rounded transition  ${colorClasses[colorVariant]}`}
+  {title}
 >
   <svelte:component this={icon} size={sizeVariant === 'small' ? 16 : 20} />
   <slot />
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,8 +19,19 @@ module.exports = {
         background: colors.slate[100],
         'on-background': colors.slate[400],
 
-        surface: colors.slate[50],
-        'on-surface': colors.slate[400],
+        surface: colors.white,
+        'on-surface': colors.slate[600],
+
+        // Status
+        destructive: colors.red[200],
+        'on-destructive': colors.slate[700],
+        'hover-destructive': colors.red[400],
+        'hover-on-destructive': colors.slate[800],
+
+        interactive: colors.cyan[200],
+        'on-interactive': colors.slate[700],
+        'hover-interactive': colors.cyan[400],
+        'hover-on-interactive': colors.slate[800],
       },
     },
   },
