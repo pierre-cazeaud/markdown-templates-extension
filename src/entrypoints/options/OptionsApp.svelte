fb2a30cba4cfe5f4660cad9a651b3a27295909df
Pierre Cazeaud <pcazeaud@turbulent.ca>
2024-03-25 17:44:39 -0400

    refactor: Rename App to OptionsApp and PopupApp

--- src/entrypoints/options/OptionsApp.svelte
+++ src/entrypoints/options/OptionsApp.svelte
@@ -1,26 +1,23 @@
 <script lang="ts">
   import Button from '@/lib/components/Button.svelte';
   import Container from '@/lib/layouts/Container.svelte';
+  import EditPage from '@/lib/layouts/EditPage.svelte';
+  import ListPage from '@/lib/layouts/ListPage.svelte';
   import NoTemplate from '@/lib/layouts/NoTemplate.svelte';
   import { ArrowDownToLineIcon, CogIcon, PlusIcon } from 'lucide-svelte';
   // import svelteLogo from '../../assets/svelte.svg';
 
-  const items = undefined;
+  let isEditing = $state(false);
+  let isEditingId = undefined;
 </script>
 
 <Container isFullWidth>
-  {#if !items}
-    <NoTemplate />
+  {#if !isEditing}
+    <ListPage
+      onCreateClick={() => (isEditing = true)}
+      onImportClick={() => console.log('Import')}
+    />
   {:else}
-    <h1 class="text-xl">Templates</h1>
+    <EditPage onBackClick={() => (isEditing = false)} />
   {/if}
-
-  <footer class="flex justify-center gap-4 mt-auto">
-    <Button icon={ArrowDownToLineIcon} onClick={() => console.log('Import')}
-      >Import template</Button
-    >
-    <Button icon={PlusIcon} onClick={() => console.log('Create')}
-      >Create template</Button
-    >
-  </footer>
 </Container>
--- src/entrypoints/options/main.ts
+++ src/entrypoints/options/main.ts
@@ -1,6 +1,6 @@
 import './app.css';
 import { mount } from 'svelte';
-import App from './App.svelte';
+import App from './OptionsApp.svelte';
 
 // @ts-ignore
 const app = mount(App, { target: document.getElementById('app') });
