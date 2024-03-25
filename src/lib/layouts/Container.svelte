81e3c73a22e0394d6790c6274fa9a608b4fe6e51
Pierre Cazeaud <pcazeaud@turbulent.ca>
2024-03-25 17:42:58 -0400

    feat: Update container styles, add body classes

--- src/lib/layouts/Container.svelte
+++ src/lib/layouts/Container.svelte
@@ -4,10 +4,12 @@
   };
 
   let { isFullWidth }: Props = $props();
+
+  document.body.classList.add('bg-background', 'text-on-background');
 </script>
 
 <main
-  class="flex flex-col gap-4 min-h-[600px] min-w-[600px] max-w-screen-2xl w-full p-4 bg-background text-on-background"
+  class="flex flex-col gap-4 min-h-[600px] min-w-[600px] max-w-screen-2xl mx-auto w-full p-4"
   class:min-h-screen={isFullWidth}
 >
   <slot />
