4c055010df3d9c2220167121202616ad17bacb83
Pierre Cazeaud <pierre.cazeaud@gmail.com>
2024-04-15 17:25:32 -0400

    feat: Create TextInput

---
+++ src/lib/components/TextInput.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { HTMLAttributes } from 'svelte/elements';
+
+  type Props = HTMLAttributes<HTMLInputElement>;
+
+  let { class: classes, ...props }: Props = $props();
+</script>
+
+<input
+  class={`border bg-surface text-on-surface rounded text-base p-2  ${classes}`}
+  type="text"
+  {...props}
+/>
