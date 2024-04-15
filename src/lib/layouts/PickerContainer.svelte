c485060cdca298a9c3a5c04cc52b2bd29f8119e7
Pierre Cazeaud <pierre.cazeaud@gmail.com>
2024-04-15 17:39:42 -0400

    fix: PickerContainer selection

---
+++ src/lib/layouts/PickerContainer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Label from '../components/Label.svelte';
+
+  type Props = {
+    title: string;
+  };
+
+  let { title }: Props = $props();
+</script>
+
+<div
+  class="flex flex-col gap-4 p-4 rounded-lg bg-surface text-on-surface border"
+>
+  <Label class="grow">{title}</Label>
+
+  <div class="flex flex-wrap gap-4">
+    <slot />
+  </div>
+</div>
