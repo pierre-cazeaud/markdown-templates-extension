9326ff974fc9091e05dd9439ae6f130894cf7f7b
Pierre Cazeaud <pierre.cazeaud@gmail.com>
2024-04-15 17:25:20 -0400

    feat: Create Label

---
+++ src/lib/components/Label.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { HTMLAttributes } from 'svelte/elements';
+
+  type Props = HTMLAttributes<HTMLParagraphElement>;
+
+  let { class: classes, ...props }: Props = $props();
+</script>
+
+<p class={`text-base font-semibold ${classes}`} {...props}>
+  <slot />
+</p>
