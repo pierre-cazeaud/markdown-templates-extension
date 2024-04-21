89f42a2241d10412fe0e614099a99ebf561555ef
Pierre Cazeaud <pcazeaud@turbulent.ca>
2024-04-21 00:33:38 -0400

    feat: Uniformize container padding, use @render for children

--- src/lib/layouts/Container.svelte
+++ src/lib/layouts/Container.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
+  import type { Snippet } from 'svelte';
+
   type Props = {
+    children: Snippet,
     isFullWidth?: boolean;
   };
 
-  let { isFullWidth }: Props = $props();
+  let { children, isFullWidth }: Props = $props();
 
   document.body.classList.add('bg-background', 'text-on-background');
 </script>
 
 <main
-  class="flex flex-col gap-4 min-h-[600px] min-w-[600px] max-w-screen-2xl mx-auto w-full p-4 px-8"
+  class="flex flex-col gap-4 min-h-[600px] min-w-[600px] max-w-screen-2xl mx-auto w-full p-8"
   class:min-h-screen={isFullWidth}
 >
-  <slot />
+  {@render children()}
 </main>
