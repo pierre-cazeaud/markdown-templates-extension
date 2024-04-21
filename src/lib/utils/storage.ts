f18a5d1258c705445341f7131696f04756ee4f7d
Pierre Cazeaud <pcazeaud@turbulent.ca>
2024-04-20 22:20:17 -0400

    fix: Storage condition being too permissive

--- src/lib/utils/storage.ts
+++ src/lib/utils/storage.ts
@@ -1,5 +1,5 @@
 import type { StoredTemplatesData } from '../types';
-import browser from "webextension-polyfill";
+import browser from 'webextension-polyfill';
 
 // Commented out because causing an issue when saving an array.
 // Array becomes an object with key/value using array index and array value at index.
@@ -14,25 +14,31 @@ import browser from "webextension-polyfill";
 //     templatesStorage.setValue(newData);
 //   };
 
-const STORAGE_KEY = 'templatesData'
+const STORAGE_KEY = 'templatesData';
 
-export const getTemplatesStorage = async (): Promise<StoredTemplatesData | undefined> => {
+export const getTemplatesStorage = async (): Promise<
+  StoredTemplatesData | undefined
+> => {
   try {
     const data = await browser.storage.sync.get(STORAGE_KEY);
-    return JSON.parse(data?.[STORAGE_KEY]);
+    if (data?.[STORAGE_KEY]) return JSON.parse(data?.[STORAGE_KEY]);
   } catch (error) {
-    console.error('An error occured while getting templates using the storage API');
+    console.error(
+      'An error occured while getting templates using the storage API'
+    );
     console.error(error);
   }
-}
+};
 
 export const setTemplatesStorage = async (data: StoredTemplatesData) => {
   try {
     return await browser.storage.sync.set({
-      [STORAGE_KEY]: JSON.stringify(data)
-    })
+      [STORAGE_KEY]: JSON.stringify(data),
+    });
   } catch (error) {
-    console.error('An error occured while setting templates using the storage API');
+    console.error(
+      'An error occured while setting templates using the storage API'
+    );
     console.error(error);
   }
-}
+};
