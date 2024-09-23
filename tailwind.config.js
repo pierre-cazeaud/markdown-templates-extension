46aeeb2d266611e8762bd6767123fcbf313bad47
Pierre Cazeaud <pcazeaud@turbulent.ca>
2024-09-22 22:07:58 -0400

    feat: Add @tailwindcss/container-queries plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -60,5 +60,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/container-queries'),
+    require('@tailwindcss/typography'),
+  ],
 };
