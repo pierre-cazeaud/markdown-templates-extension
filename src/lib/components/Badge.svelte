bf74dbdbe5edf4677589610255bf8a9b98e5106a
Pierre Cazeaud <pcazeaud@turbulent.ca>
2024-09-24 17:49:08 -0400

    feat: Create Badge component

---
+++ src/lib/components/Badge.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import type { HTMLAttributes } from 'svelte/elements';
+  import { fly } from 'svelte/transition';
+
+  type ColorVariant = 'destructive' | 'interactive' | 'neutral' | 'successful';
+
+  type Props = HTMLAttributes<HTMLSpanElement> & {
+    colorVariant?: ColorVariant;
+    text: string;
+  };
+
+  let {
+    class: classes,
+    colorVariant = 'neutral',
+    text,
+    ...props
+  }: Props = $props();
+
+  const colorClasses: Record<ColorVariant, string> = {
+    destructive: 'border-destructive',
+    interactive: 'border-interactive',
+    neutral: 'border-neutral',
+    successful: 'border-successful',
+  };
+</script>
+
+<span
+  class={`flex justify-center items-center size-5 rounded-full border bg-surface text-on-surface text-xs ${colorClasses[colorVariant]} ${classes || ''}`}
+  transition:fly={{ y: 4 }}
+  {...props}
+>
+  {text}
+</span>
