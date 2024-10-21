d3959cde460f046222cd8fa5f3ff36bff6be151d
Pierre Cazeaud <pcazeaud@turbulent.ca>
2024-10-21 15:02:22 -0400

    fix: Ensure Page sticky header is above content

--- src/lib/layouts/Page.svelte
+++ src/lib/layouts/Page.svelte
@@ -15,7 +15,7 @@
 <div class={`flex flex-col grow gap-4 ${classes || ''}`} in:fade={{duration: 150}}>
   {#if header}
     <!-- empty:hidden is used as a work around because a snippet can not be wrapped in a if statement and passed down, only its content (see List) -->
-    <header class="sticky top-2 flex justify-between items-center w-full p-2 bg-surface/30 text-on-surface backdrop-blur-sm shadow-sm rounded empty:hidden">
+    <header class="sticky top-2 z-10 flex justify-between items-center w-full p-2 bg-surface/30 text-on-surface backdrop-blur-sm shadow-sm rounded empty:hidden">
       {@render header()}
     </header>
   {/if}
